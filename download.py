f38242aef27b23af3578b39d84d61783a8c714e2
ofilhi <ofotaku2001@gmail.com>
2024-09-06 15:22:51 +0200

    adding model integration and conversion to ollama

---
+++ download.py
@@ -0,0 +1,4 @@
+from huggingface_hub import snapshot_download
+model_id="meta-llama/Meta-Llama-3-8B-Instruct"
+snapshot_download(repo_id=model_id, local_dir="llama",
+                  local_dir_use_symlinks=False, revision="main")
