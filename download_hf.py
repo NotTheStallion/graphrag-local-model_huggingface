7ff87d0b4ff2fccfaf947d59c83242baddb50749
The_Stallion <111058733+NotTheStallion@users.noreply.github.com>
2024-09-09 12:04:02 +0200

    Adding download model from huggingface code.

--- download_hf.py
+++ download_hf.py
@@ -1,4 +1,4 @@
 from huggingface_hub import snapshot_download
 model_id="meta-llama/Meta-Llama-3-8B-Instruct"
-snapshot_download(repo_id=model_id, local_dir="llama",
+snapshot_download(repo_id=model_id, local_dir="custom_model",
                   local_dir_use_symlinks=False, revision="main")
